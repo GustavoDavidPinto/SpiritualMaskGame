39b84b404654a6a0d0f8f7fbe5707ffd20a369f2
Gustavo David <gustavodavidpinto@gmail.com>
2026-04-16 21:33:39 -0300

    Add Inputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,19 +1,44 @@
 package main;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
-    public GamePanel() {
+import main.inputs.KeyboardInputs;
+import main.inputs.MouseInputs;
 
+public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
+
+    public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    public void changeXDelta(int value) {
+
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+
+        this.yDelta += value;
+        repaint();
+
+    }
+
+    public void setRecPos(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(100, 100, 200, 50);
-        g.setColor(Color.red);
-        g.drawLine(0, 0, 50, 50);
+        g.fillRect(xDelta, yDelta, 200, 50);
     }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -7,9 +7,10 @@ public class GameWindow extends JFrame {
 
     public GameWindow(GamePanel gamePanel) {
         jframe = new JFrame();
-        jframe.setSize(1920, 1080);
+        jframe.setSize(400, 400);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
 
     }
